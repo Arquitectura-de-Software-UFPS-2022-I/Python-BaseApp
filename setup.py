5c4924c97401ffb48e7e3d141c2d62388df5c2db
ManuelCoronelAndrade <andrademanuelalejandroc@ufps.edu.co>
2022-03-06 12:49:20 -0500

    Se creo la libreria

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import find_packages, setup
 setup(
     name='signaturelib',
     packages=find_packages(include=['signaturelib']),
-    version='0.1.2',
+    version='1.0.0',
     description='Base application for signature',
     author='Python-Group-UFPS',
     license='MIT',
-    install_requires=['sqlalchemy' ,'pymysql','pymupdf', 'requests'],
+    install_requires=['requests'],
 )
